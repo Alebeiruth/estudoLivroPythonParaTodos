a471a2ddc93efccb7a65274de44000845da92d05
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2024-01-23 22:47:23 -0300

    Update estudoPythonLivro.py

--- estudoPythonLivro.py
+++ estudoPythonLivro.py
@@ -2,13 +2,13 @@
 ## CAPITULO 02
 ## 01 - USO DE VARIAVEIS E SEU TIPO
 
-"""print(type('Hello World!'))
+print(type('Hello World!'))
 print(type(17))
-print(type(17.5))"""
+print(type(17.5))
 
 ## 02 - OPERADORES, EXPRESSÕES, OPERANDOS
 
-"""soma = 20 + 32
+soma = 20 + 32
 hora = 60 - 1
 minutos = 30
 horasEminutos = hora * 60 * minutos
@@ -16,7 +16,7 @@ divisaoHorasMinutos = minutos / 60
 exponencia = 5 ** 2
 divisaoInt = minutos // 60
 
-print(f"Soma = {soma}, Subtração = {hora}, Multiplicação = {horasEminutos}, Divisão =  {divisaoHorasMinutos},  Exponenciação =  {exponencia}, Divisao Inteira = {divisaoInt}")"""
+print(f"Soma = {soma}, Subtração = {hora}, Multiplicação = {horasEminutos}, Divisão =  {divisaoHorasMinutos},  Exponenciação =  {exponencia}, Divisao Inteira = {divisaoInt}")
 
 ## ORDEM DAS OPREAÇÕES >> PEMDAS
 ## 1 - PARENTESES
@@ -26,26 +26,26 @@ print(f"Soma = {soma}, Subtração = {hora}, Multiplicação = {horasEminutos}, 
 
 ## 03 - OPERADOR DE MODULO >> QUOCIENTE E RESTO
 
-"""quociente = 7 // 3
+quociente = 7 // 3
 print(quociente)
 
 resto = 7 % 3
-print(resto)"""
+print(resto)
 
 ## 04 - OPERANDO COM STR
-"""
+
 first = "106"
 second = "901"
 print(first + second)
 
 testTres = "Teste "
 tresVzs = 3
-print(tresVzs * testTres)"""
+print(tresVzs * testTres)
 
 ## 05 - REQUISITANDO ENTRADA(VALORES) AO USER
 
-"""nome = str(input("Qual seu nome: "))
-print(f"Seu nome é {nome}! ")"""
+nome = str(input("Qual seu nome: "))
+print(f"Seu nome é {nome}! ")
 
 ## Erro de Syntax >> palavra escrita incorretamente, não segue regras gramaticais do Py
 ## Erro de Name >> nome da variavel está diferente ou não definido no escopo do programa
@@ -54,41 +54,41 @@ print(f"Seu nome é {nome}! ")"""
 ## EXERCICIOS CAPITULO 2
 ##2.2
 
-"""nome = str(input("Qual seu nome: "))
-print(f"Olá {nome}")"""
+nome = str(input("Qual seu nome: "))
+print(f"Olá {nome}")
 
 ##2.3
 
-"""
+
 hora = int(input("Quantas horas voce trabalha ao dia: "))
 taxa = float(input("Qual a taxa: "))
 print(f"Valor a ser pago: {hora * taxa.__round__()}")
-"""
+
 
 ##2.4
 
-"""largura = 17
+largura = 17
 altura = 12.0
 
-print(f"Quociente = {largura //2}, Divisão = {largura / 2.0}, Divisao Interia = {altura / 3}, Multiplicação e Soma = {1 + (2 * 5)} ")"""
+print(f"Quociente = {largura //2}, Divisão = {largura / 2.0}, Divisao Interia = {altura / 3}, Multiplicação e Soma = {1 + (2 * 5)} ")
 
 ##2.5
 
-"""celsius = float(input("Qual a temperatura de hoje em Ceslius ? Digite ao lado: "))
+celsius = float(input("Qual a temperatura de hoje em Ceslius ? Digite ao lado: "))
 print(f"A temperatura em Fahrenheit é {(celsius * 9/5) + 32} graus")
-"""
+
 
 ##LIVRO > PYTHON PARA TODOS, SEVERANCE CHARLES
 ## CAPITULO 03
 ## 06 - EXPRESSÃO BOLEANAS
 
-"""five = 5 == 5
+five = 5 == 5
 noFive = 5 == 6
 print(five)
 print(noFive)
-"""
 
-"""x = 1
+
+x = 1
 y = 2
 
 print(x is y)
@@ -97,49 +97,49 @@ print(x != y) ##diferente
 print(x > y)
 print(x < y)
 print(x >= y)
-print(x <= y)"""
+print(x <= y)
 
 ## 07 - OPERADORES LOGICOS >> AND, OR , NOT
 
-"""
+
 x = 1
 y = 3
 n = 4
 print(x > 0 and y < 10)
 print(n % 2 == 0 or n % 3 == 0)
 print(not(x > y))
-"""
+
 
 ## 08 - EXECUÇÃO DE CONSDICIONAL
 ## IF
 
-"""a = 10
+a = 10
 if a > 0:
-    print(f"A = {a} é um número positivo")"""
+    print(f"A = {a} é um número positivo")
 
 ## IF e ELSE
 
-"""a = 4
+a = 4
 
 if a % 2 == 0:
     print(f"O número {a} é par.")
 else:
-    print("O número é impar")"""
+    print("O número é impar")
 
 ## 09 - CONDIÇÃO ENCADEADA
 
-"""x = 2
+x = 2
 y = 1
 if x < y:
     print(f"O número {x} é menor que {y}")
 elif x > y:
     print(f"O número {x} é maior que {y}")
 else:
-    print(f"O número {x} é {y} são iguais")"""
+    print(f"O número {x} é {y} são iguais")
 
 ## 10 - CONDIÇÃO ANINHADAS
 ## TIPO DE ESTRUTURA A SER EVITADA
-"""a = 3
+a = 3
 b = 7
 
 if a == b:
@@ -148,39 +148,39 @@ else:
     if a < b:
         print(f"{a} é menor que {b}")
     else:
-        print(f"{a} é maior que {b}")"""
+        print(f"{a} é maior que {b}")
 
 ## RESCRIÇÃO FACILITADA
 
-"""w = 5
+w = 5
 if 0 < w and w < 10:
-    print(f"{w} é um numero positivo e só possui um digito")"""
+    print(f"{w} é um numero positivo e só possui um digito")
 
 ## 11 - TRY E EXCEPT
 
-"""inp = input("Enter com a temperatura em Fahrenheit: ")
+inp = input("Enter com a temperatura em Fahrenheit: ")
 try:
     fahr = float(inp)
     cels = (fahr - 32.0) * 5.0 / 9.0
     print(cels)
 except:
-    print("Por favor entre com um número")"""
+    print("Por favor entre com um número")
 
 ## EXERCICIOS CAPITULO 3
 ##3.1
 
-"""hora = int(input("Quantas horas voce trabalha ao dia: "))
+hora = int(input("Quantas horas voce trabalha ao dia: "))
 taxa = float(input("Qual a taxa: "))
 print(f"Valor a ser pago: {hora * taxa.__round__()}")
 
 if hora >= 40:
     print((hora * taxa) * 1.5)
 else:
-    print(hora * taxa)"""
+    print(hora * taxa)
 
 ##3.2
 
-"""entrada = input("Digite as horas: ")
+entrada = input("Digite as horas: ")
 tax = input("Digite a taxa: ")
 
 try:
@@ -188,7 +188,7 @@ try:
     intTax = int(tax)
     print(intEntrada * intTax)
 except:
-    print("Erro, por favor utilize uma entrada numérica")"""
+    print("Erro, por favor utilize uma entrada numérica")
 
 ##3.3
 
